5f27c437ef1e6c0ef428410dbf29e39b8aa667f0
VIKAS SHARMA <59640904+VIKAS-WEB@users.noreply.github.com>
2026-02-03 19:16:17 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 //http://13.126.236.113:5000
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://13.126.236.113",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
